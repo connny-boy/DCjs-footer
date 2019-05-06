ab7a9a7ef637ce31296e5ba78b6fe0e51fc92a71
Conner Nielson <nielsoncp@msn.com>
2019-05-06 13:35:06 -0600

    second commit

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var moment = require('moment');
+
+/**
+ * Returns a string element with a footer and updating year
+ * @param {string} name
+ * @return {string}
+ */
+exports.footer = function (name) {
+    return "Copyright " + moment().format('YYYY') + " " + name + " All rights reserved";
+};
